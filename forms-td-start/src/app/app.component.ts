605cd2d1731aac9771ba6faa23a237fd5f29ae39
Ayush Goyal <ayush987goyal@gmail.com>
2017-06-04 01:42:55 +0530

    form lessons final

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupform: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupform.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupform.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
+
+  // onSubmit(form: NgForm){
+  //   console.log(form);
+  // }
+
+  onSubmit(){
+    this.user.username = this.signupform.value.userData.username;
+    this.user.email = this.signupform.value.userData.email;
+    this.user.secretQuestion = this.signupform.value.secret;
+    this.user.answer = this.signupform.value.questionAnswer;
+    this.user.gender = this.signupform.value.gender;
+    this.submitted = true;  
+
+    this.signupform.reset();
+}
 }
