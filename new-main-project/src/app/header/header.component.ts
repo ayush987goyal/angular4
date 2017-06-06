bd2dcb322f9a5bddb9d043244ea8e6aa3512a356
Ayush Goyal <ayush987goyal@gmail.com>
2017-06-07 01:54:26 +0530

    Final commit after adding HTTP section in main project

--- new-main-project/src/app/header/header.component.ts
+++ new-main-project/src/app/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { DataStorageServcie } from '../shared/data-storage.service';
+import { Response } from '@angular/http';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    
+    constructor(private dataStorageServcie: DataStorageServcie){}
+
+    onSaveData(){
+        this.dataStorageServcie.storeRecipes().subscribe(
+            (response: Response) => {console.log(response)}
+        );
+    }
+
+    onFetchData(){
+        this.dataStorageServcie.getRecipes();
+    }
 }
